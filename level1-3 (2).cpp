2e7233849f455c21b8e1b09855ba605c0b093dd8
Helmet181 <1729036144@qq.com>
2024-09-20 16:15:10 +0800

    Add files via upload

--- level1-3 (2).cpp
+++ level1-3 (2).cpp
@@ -2,15 +2,17 @@
 
 int main()
 {
-
+	int choice=1;                         //choice表示用户是否要继续购买 
 	char goods;							//goods表示货物 
     int univalence;						//univalence表示单价 
 	int channel;						//channel表示通道
-	int number;							//number表示货物摆放个数
+	int number; 						//number表示货物摆放个数
+	char c;							
+	while(choice==1){
 	printf ("请依次输入货物种类、通道编号、货物单价以及货物个数:\n");
 	scanf("%c %d %d %d",&goods,&channel,&univalence,&number); 
 	
-	while(getchar()!='\n'); 
+	while(getchar()!='\n');                 //清理一下缓冲区 
 	int umbrella;							//umbrella表示需要买几个 
 	
 	printf("请输入需要购买的货物、货物所在通道以及购买数量:\n");
@@ -25,6 +27,24 @@ int main()
 		scanf("%d",&i);
 	} 
 	 int change=money-amount;
-	 printf("找零%d元",change);
-	 return 0; 
+	 printf("找零%d元\n",change);
+	 
+	 printf("要不要再来点？\n");
+	 printf("y or n\n") ;   //y代表yes，n代表no 
+	while(getchar()!='\n'); 
+	scanf("%c",&c);
+	if (c=='y'){
+		choice=1;
+		}
+	else if(c=='n'){
+		return 0;
+	}
 }
+	
+	     return 0; 
+	} 
+	 
+	
+
+
+
